7ad14c3a11b3f406fb7e9fb4ad075f5315251a04
Satheesh-R <satheesh.rsm1@gmail.com>
2021-04-25 23:50:57 +0530

    connected with backend(spring boot app)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <TeamPage/>
+        <Router>
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+        </Router>
       </header>
     </div>
   );
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,29 +1,34 @@
-import {React, useEffect, useState} from 'react';
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
-    
-    const [team, setTeam] = useState({matches:[]});
 
+    const [team, setTeam] = useState({ matches: [] });
+    const { teamName } = useParams();
     useEffect(
         () => {
-            const fetchMatches = async() => {
-                const response = await fetch('http://localhost:8080/team/Chennai Super Kings');
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
+                console.log(data);
             };
             fetchMatches();
         },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         []
     );
-    
-    
+
+    if (!team || !team.teamName) {
+        return <h1>Team not Found</h1>
+    }
     return (
         <div className="TeamPage">
             <h1>{team.teamName}</h1>
             <MatchDetailCard match={team.matches[0]} />
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+            {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
         </div>
     );
 }
