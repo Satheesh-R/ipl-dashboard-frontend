e7d0b204596278b005e388ad12546f8845cf2cb6
Satheesh-R <satheesh.rsm1@gmail.com>
2021-04-26 20:24:46 +0530

    match page skelton for teams added

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -1,13 +1,28 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+    const [matches, setMatches] = useState([]);
+    const {teamName,year} = useParams();
+    useEffect(
+        () => {
+            const fetchMatchesByYearandTeam = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                setMatches(data);
+                console.log(data);
+            };
+            fetchMatchesByYearandTeam();
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    );
     return (
         <div className="MatchPage">
-            <h1>Match page here!</h1>
+            <h1>Match Page</h1>
+            {matches.map(match => <MatchDetailCard match={match} teamName={teamName} />)}
         </div>
     );
 }
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -13,7 +13,6 @@ export const TeamPage = () => {
                 const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
-                console.log(data);
             };
             fetchMatches();
         },
