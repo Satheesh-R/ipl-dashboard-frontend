f1eb6b7cad3f0666d2011dbf353417e35535d8b5
Satheesh-R <satheesh.rsm1@gmail.com>
2021-05-02 00:14:33 +0530

    match cards styling completed

--- src/components/MatchSmallCard.js
+++ src/components/MatchSmallCard.js
@@ -1,13 +1,16 @@
 import {React} from 'react';
 import {Link} from 'react-router-dom';
+import './MatchSmallCard.scss';
 
 export const MatchSmallCard = ({teamName,match}) => {
     if(!match) return null;
     const otherTeam = teamName === match.team1 ? match.team2 : match.team1;
+    const isMatchWon = teamName === match.matchWinner;
     return(
-        <div className="MatchSmallCard">
-            <h3>vs <Link to={`/teams/${otherTeam}`}>{otherTeam}</Link></h3>
-            <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+        <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
+            <span>vs</span>
+            <h3> <Link to={`/teams/${otherTeam}`}>{otherTeam}</Link></h3>
+            <p className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</p>
         </div>
     );
 }   
