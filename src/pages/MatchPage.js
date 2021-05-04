cbfd5becb1308a50171e6dabdb1f3f7eacac67d7
Satheesh-R <satheesh.rsm1@gmail.com>
2021-05-04 17:28:32 +0530

    styling completed

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -1,28 +1,35 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
+import { YearSelector } from '../components/YearSelector';
+import './MatchPage.scss'
 
 export const MatchPage = () => {
 
     const [matches, setMatches] = useState([]);
-    const {teamName,year} = useParams();
+    const { teamName, year } = useParams();
     useEffect(
         () => {
             const fetchMatchesByYearandTeam = async () => {
-                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/team/${teamName}/matches?year=${year}`);
                 const data = await response.json();
                 setMatches(data);
-                console.log(data);
             };
             fetchMatchesByYearandTeam();
         },
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        []
+        [teamName,year]
     );
     return (
         <div className="MatchPage">
-            <h1>Match Page</h1>
-            {matches.map(match => <MatchDetailCard match={match} teamName={teamName} />)}
+            <div className="year-selector">
+                <h2>Select Year</h2>
+                <YearSelector teamName={teamName} />
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {matches.map(match => <MatchDetailCard key={match.id} match={match} teamName={teamName} />)}
+            </div>
         </div>
     );
 }
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { PieChart } from 'react-minimal-pie-chart';
@@ -11,12 +11,12 @@ export const TeamPage = () => {
     const { teamName } = useParams();
     useEffect(
         () => {
-            const fetchMatches = async () => {
-                const response = await fetch(`http://localhost:8080/team/${teamName}`);
+            const fetchTeam = async () => {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
             };
-            fetchMatches();
+            fetchTeam();
         },
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [teamName]
@@ -36,7 +36,7 @@ export const TeamPage = () => {
                     data={[
                         { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
                         { title: 'Wins', value: team.totalWins, color: '#4da375' },
-                        
+
                     ]}
                 />
             </div>
@@ -44,9 +44,11 @@ export const TeamPage = () => {
                 <h3>Latest Matches</h3>
                 <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
             </div>
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} teamName={team.teamName} />)}
+            {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} match={match} teamName={team.teamName} />)}
             <div className="more-link">
-                <a href="/moreMatches">More &gt;</a>
+                <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>
+                    More &gt;
+                </Link>
             </div>
         </div>
     );
